205c14e8b58abb732a6ea492faf95e1ad7d07421
Deepu-000 <83580853+Deepu-000@users.noreply.github.com>
2021-09-10 20:02:14 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -109,8 +109,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data=="help":
         buttons = [
             [
-                InlineKeyboardButton('⚙️ Update Channel', url='https://t.me/subin_works'),
-                InlineKeyboardButton('🧩 Source', url='https://github.com/subinps/VCPlayerBot'),
+                InlineKeyboardButton(' ⚙️Sᴜᴘᴘᴏʀᴛ ', url='<b>Gᴏ ᴀɴᴅ Fᴜᴄᴋ</b>'),
+                InlineKeyboardButton('🧩Sᴏᴜʀᴄᴇ ', url='<b>ɴᴏᴋᴋᴋɪ ᴇʀʀᴜɴɴɴᴏ ɪᴘᴏᴍ ᴋɪᴛᴛᴜᴍ</b>'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
